c6eed293dc93767e887dcf21e3c08713134dec8f
Ilmo10 <necok_cley@hotmail.com>
2022-05-03 09:23:56 -0300

    criando github para atividades JS

---
+++ meu_script.js
@@ -0,0 +1,8 @@
+let nota = prompt('Digite a nota do aluno: ')
+let media = 7
+
+if(nota >= media){
+  document.write( "Sua nota é: " + nota + " você passou!!")
+}else{
+  document.write( "Sua nota é: " + nota + " você não passou!!")
+}
