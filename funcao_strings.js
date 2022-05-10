7981621a44e6dcef44f56a1b6d909c90dce53b3b
Ilmo10 <necok_cley@hotmail.com>
2022-05-09 22:33:06 -0300

    Strings

---
+++ funcao_strings.js
@@ -0,0 +1,25 @@
+
+
+// Site Ref: w3schools.com
+
+
+
+
+
+// propiedade length
+//document.write("Jorge Sant Ana".length)
+
+
+//document.write("Jorge Sant Ana".charAt(7))
+
+let nome = " Jorge Sant Ana "
+
+//document.write(nome.indexOf('g'))
+
+//document.write(nome.replace('Sant Ana', 'Silva ------'))
+
+//document.write(nome.substr(6, 4))
+
+//document.write(nome.toLowerCase())
+//document.write(nome.toUpperCase())
+document.write('-' + nome.trim() + '-')
