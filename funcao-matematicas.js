18474d6d09f96b986e17a676480d894d281ae6a8
Ilmo10 <necok_cley@hotmail.com>
2022-05-09 22:53:48 -0300

    funcao matematicas

---
+++ funcao-matematicas.js
@@ -0,0 +1,18 @@
+
+
+
+// arredonda para cima
+let x = Math.floor(10.380)
+document.write(x)
+
+// arredonda para baixo
+let y = Math.ceil(10.380)
+document.write(y)
+
+// arredonda para menos de 500 arredonda para baixo, mais de 500 para cima
+let z = Math.round(10.680)
+document.write(z)
+
+// Aparece numero randomico(aleatoria)
+let I = Math.random()
+document.write(I)
