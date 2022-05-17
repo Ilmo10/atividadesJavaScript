2f79541d722836e2ac142f4ad358ae74aec7e4f7
Ilmo10 <necok_cley@hotmail.com>
2022-05-16 23:56:30 -0300

    finalizando game mata mosquito

--- app-mata-mosquito/jogo.js
+++ app-mata-mosquito/jogo.js
@@ -3,6 +3,17 @@ let altura = 0
 let largura = 0
 let vidas = 1
 let tempo = 15
+let criaMoscaTempo = 1500
+
+let nivel = window.location.search
+nivel = nivel.replace('?', '')
+if(nivel === 'normal'){
+  criaMoscaTempo = 1500
+}else if(nivel === 'dificil'){
+  criaMoscaTempo = 1000
+}else if(nivel === 'chucknorris'){
+  criaMoscaTempo = 750
+}
 
 function ajustaTamanhoPalcoJogo(){
      altura = window.innerHeight
