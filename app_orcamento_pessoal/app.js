832e38db75527163037236659e8a279d0473111f
Ilmo10 <necok_cley@hotmail.com>
2022-05-30 22:50:37 -0300

    app( orçamento pessoal) pegando os dados do usuario e adicionando via class

--- app_orcamento_pessoal/app.js
+++ app_orcamento_pessoal/app.js
@@ -9,6 +9,30 @@ class Despesas{
   }
 }
 
+class Bd {
+  constructor(){
+    let id = localStorage.getItem('id')
+
+    if(id === null) {
+      localStorage.setItem('id', 0)
+    }
+  }
+
+  getProximoId() {
+    let proximoId = localStorage.getItem('id')
+    return parseInt(proximoId) + 1
+  }
+   
+  gravar(d) {
+    let id = this.getProximoId()
+     
+    localStorage.setItem(id, JSON.stringify(d))
+
+    localStorage.setItem('id', id)
+  }
+}
+let bd = new Bd()
+
 function cadastrarDespesa(){
 
   let ano = document.getElementById('ano')
@@ -26,9 +50,6 @@ function cadastrarDespesa(){
       descricao.value, 
       valor.value
       )
-      gravar(despesa)
+      bd.gravar(despesa)
 }
 
-function gravar(d) {
-  localStorage.setItem('despesa', JSON.stringify(d))
-}
